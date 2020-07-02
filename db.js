91a6514af3a09ef750eaf4fc4ae703f305eb1453
Issue哥 <i@viegg.com>
2020-07-02 13:08:24 +0800

    turn on WAL mode for sqlite

--- db.js
+++ db.js
@@ -2,4 +2,6 @@ const path = require('path')
 const db_location = path.join(__dirname, 'gdurl.sqlite')
 const db = require('better-sqlite3')(db_location)
 
+db.pragma('journal_mode = WAL')
+
 module.exports = { db }
