cbcdeb52e83e31f69ddbc8586e6c7cedd5f69e84
liaojack8 <liaojack8@yahoo.com.tw>
2020-07-05 22:02:58 +0800

    add forceRefresh=true when call gtoken.getToken by @iwestlin

--- db.js
+++ db.js
@@ -3,3 +3,17 @@ const db_location = path.join(__dirname, 'gdurl.sqlite')
 const db = require('better-sqlite3')(db_location)
 
 module.exports = { db }
+
+create_table_copied()
+function create_table_copied () {
+  const [exists] = db.prepare('PRAGMA table_info(copied)').all()
+  // console.log('exists', exists)
+  if (exists) return
+  const create_table = `CREATE TABLE "copied" (
+  "taskid"  INTEGER,
+  "fileid"  TEXT
+)`
+  db.prepare(create_table).run()
+  const create_index = `CREATE INDEX "copied_taskid" ON "copied" ("taskid");`
+  db.prepare(create_index).run()
+}
