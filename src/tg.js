1f6715d785a0a73cedabde3b47ffd82e4d1a07a6
Issue哥 <i@viegg.com>
2020-07-21 17:46:03 +0800

    try extract url from message body

--- src/tg.js
+++ src/tg.js
@@ -368,7 +368,8 @@ function extract_fid (text) {
 }
 
 function extract_from_text (text) {
-  const reg = /https?:\/\/drive.google.com\/[^\s]+/g
+  // const reg = /https?:\/\/drive.google.com\/[^\s]+/g
+  const reg = /https?:\/\/drive.google.com\/[a-zA-Z0-9_\/?=&-]+/g
   const m = text.match(reg)
   return m && extract_fid(m[0])
 }
