cfc4d48ab88ac7736a63923d20e2a632c01c1ae3
Issue哥 <i@viegg.com>
2020-07-23 14:37:35 +0800

    only show recent 100 task records in case of message too long

--- src/tg.js
+++ src/tg.js
@@ -186,8 +186,8 @@ async function send_all_tasks (chat_id) {
     // const description = err.response && err.response.data && err.response.data.description
     // if (description && description.includes('message is too long')) {
     if (true) {
-      const text = [headers].concat(records).map(v => v.join('\t')).join('\n')
-      return sm({ chat_id, parse_mode: 'HTML', text: `所有拷贝任务：\n<pre>${text}</pre>` })
+      const text = [headers].concat(records.slice(-100)).map(v => v.join('\t')).join('\n')
+      return sm({ chat_id, parse_mode: 'HTML', text: `所有拷贝任务(只显示最近100条)：\n<pre>${text}</pre>` })
     }
     console.error(err)
   })
