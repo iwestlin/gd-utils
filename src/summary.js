61edfe6cb2ab21ae51e7677ce8520e1449d7fed2
Issue哥 <i@viegg.com>
2020-08-28 13:40:33 +0800

    keep folder row

--- src/summary.js
+++ src/summary.js
@@ -60,7 +60,9 @@ function make_tg_table ({ file_count, folder_count, total_size, details }, limit
     if (v.ext === '无扩展名') v.ext = '[NoExt]'
   })
   let records = details.map(v => [v.ext, v.count, v.size]).map(arr => arr.map(content => ({ content, hAlign })))
+  const folder_row = records.pop()
   if (limit) records = records.slice(0, limit)
+  records.push(folder_row)
   const total_count = file_count + folder_count
   const tails = ['Total', total_count, total_size].map(v => ({ content: v, hAlign }))
   tb.push(headers, ...records)
