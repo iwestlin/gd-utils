6c856eccf225db471e8c4e627cc0106bf3515be3
Issue哥 <i@viegg.com>
2020-11-24 04:33:39 +0800

    little fix

--- src/gd.js
+++ src/gd.js
@@ -675,7 +675,8 @@ async function copy_files ({ files, mapping, service_account, root, task_id }) {
     let { id, parent, md5Checksum } = file
     if (argv.hash_server === 'local') id = get_gid_by_md5(md5Checksum) || id
     const target = mapping[parent] || root
-    copy_file(id, target, service_account: !!argv.hash_server, null, task_id).then(new_file => {
+    const use_sa = argv.hash_server ? true : service_account
+    copy_file(id, target, use_sa, null, task_id).then(new_file => {
       if (new_file) {
         count++
         db.prepare('INSERT INTO copied (taskid, fileid) VALUES (?, ?)').run(task_id, file.id)
